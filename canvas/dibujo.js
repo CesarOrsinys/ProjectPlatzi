5eab5c27ebd6224076b851d2cda71eceb5f48a37
César Manuel Brito Orsinys <cesarorsinys@gmail.com>
2017-05-22 16:40:01 -0400

    Lección 15: Eventos & Formularios

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,26 +1,44 @@
+/*Variables de Funcionalidad*/
+var btnSend = document.getElementById("btnEnviar");
+var txtLine = document.getElementById("txtLineas");
+btnSend.addEventListener("click", dibujoPorClick);
+
+/*Variables del Lienzo*/
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var lineas=30;
-var l=0;
-var yi, xf;
+var ancho = d.width;
 
-for (l=0; l < lineas; l++)
-{
-	yi= 10 * l;
-	xf = 10 * (l+1);
-	dibujarLinea ("black", 300, yi, xf, 0);
-	dibujarLinea ("#f2d118", 0,yi,xf,300);
-}
-	dibujarLinea("#f2d118", 1, 1, 1, 300);
-	dibujarLinea("#f2d118", 1, 299, 299, 299);
-	dibujarLinea("black", 300, 300, 300, 1);
-	dibujarLinea("black", 299, 1, 1, 1);
+/*Traza las Lineas*/
 function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
+	{
+		lienzo.beginPath();
+		lienzo.strokeStyle = color;
+		lienzo.moveTo(xinicial,yinicial);
+		lienzo.lineTo(xfinal,yfinal);
+		lienzo.stroke();
+		lienzo.closePath();	
+	}
+
+/*Click click click lick*/
+function dibujoPorClick()
 {
-	lienzo.beginPath();
-	lienzo.strokeStyle = color;
-	lienzo.moveTo(xinicial,yinicial);
-	lienzo.lineTo(xfinal,yfinal);
-	lienzo.stroke();
-	lienzo.closePath();	
+	var lineas=parseInt(txtLine.value);
+	var l=0;
+	var yi, xf;
+	var colorcito="#f2d118";
+	var espacio = ancho / lineas;
+
+	/*Ciclo For para Multiples Lienas*/
+	for (l=0; l < lineas; l++)
+		{
+			yi= espacio * l;
+			xf = espacio * (l+1);
+			dibujarLinea (colorcito, 0,yi,xf,300);
+			console.log("linea " + l);
+		}
+
+	/*Recoloreado de las lineas*/
+	dibujarLinea(colorcito, 1, 1, 1, 299);
+	dibujarLinea(colorcito, 1, 299, 299, 299);
+
 }
